3da5fe227497af513d0fb55db77bf4852905b03a
Filipe Arantes Fernandes <filran@gmail.com>
2015-06-30 13:40:04 -0300

    Atualizando

--- VisualStudio/ConsoleApplication1/TesteXMI2/Program.cs
+++ VisualStudio/ConsoleApplication1/TesteXMI2/Program.cs
@@ -1,7 +1,4 @@
-﻿// >>>> LER OS ATRIBUTOS E VALORES AUTOMATICAMENTE
-
-
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,49 +14,133 @@ namespace TesteXMI2
 
         static void Main(string[] args)
         {
-            //string arquivoXmi = "F:\\Users\\Filipe\\Documents\\Programacao\\GitHub\\Behavior\\VisualStudio\\ConsoleApplication1\\TesteXMI2\\DiaSeq_XMI2.1.xmi";
-            string arquivoXmi = "F:\\Documentos\\Programacao\\GitHub\\Behavior\\VisualStudio\\ConsoleApplication1\\TesteXMI2\\DiaSeqs_XMI2.1.xml";
+            Dictionary<ArrayList, ArrayList> diagrams = new Dictionary<ArrayList, ArrayList>();
+            
+            string arquivoXmi = "F:\\Users\\Filipe\\Documents\\Programacao\\GitHub\\Behavior\\VisualStudio\\ConsoleApplication1\\TesteXMI2\\DiaSeqs_XMI2.1.xml";
+            //string arquivoXmi = "F:\\Documentos\\Programacao\\GitHub\\Behavior\\VisualStudio\\ConsoleApplication1\\TesteXMI2\\DiaSeqs_XMI2.1.xml";
 
-            XMI xmi = new XMI(arquivoXmi);
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(arquivoXmi);
 
-            ArrayList ownedList = xmi.OwnedBehavior;
-            ArrayList lifeList = xmi.Lifeline;
-            ArrayList msgList = xmi.Message;
+            XmlNodeList d = xmlDocument.SelectNodes("//diagrams");
 
-            ArrayList o = xmi.OwnedBehavior;
-            foreach (Element oo in o)
-            {
-                Console.Write("<"+oo.Tag);
-                foreach( var a in oo.AttributesElement )
-                {
-                    Console.Write(" "+a.Key+"="+a.Value+" ");
+            //diagrams
+            foreach( XmlNode c in d ){
+                Console.WriteLine( c.Name );
+                Console.WriteLine(c.ChildNodes.Count);
+
+                //diagram
+                foreach( XmlNode cc in c.ChildNodes ){
+                    Console.WriteLine( "\t"+cc.Name );
+
+                    //tags
+                    foreach (XmlNode ccc in cc.ChildNodes)
+                    {
+                        Console.WriteLine("\t\t"+ccc.Name);
+
+                        if( ccc.Name == "elements" )
+                        {
+                            foreach( XmlNode e in ccc.ChildNodes )
+                            {
+                                Console.WriteLine("\t\t\t" + e.Name);
+                            }
+                        }
+                    }
                 }
-                Console.WriteLine("/>"); 
             }
+
+
+            //foreach (XmlNode i in root.ChildNodes)
+            //{
+            //    //Console.WriteLine(i.Name);
+            //}
+
+
+            //XMI xmi = new XMI(arquivoXmi);
+
+            //ArrayList o = xmi.OwnedBehavior;
+            //foreach (Element oo in o)
+            //{
+            //    Console.Write("<"+oo.Tag);
+            //    foreach( var a in oo.AttributesElement )
+            //    {
+            //        Console.Write(" "+a.Key+"="+a.Value+" ");
+            //    }
+            //    Console.WriteLine("/>"); 
+            //}
                
-            ArrayList l = xmi.Lifeline;
-            foreach (Element ll in l)
-            {
-                Console.Write("\t<" + ll.Tag);
-                foreach (var a in ll.AttributesElement)
-                {
-                    Console.Write(" " + a.Key + "=" + a.Value + " ");
-                }
-                Console.WriteLine("/>");
-            }
+            //ArrayList l = xmi.Lifeline;
+            //foreach (Element ll in l)
+            //{
+            //    Console.Write("\t<" + ll.Tag);
+            //    foreach (var a in ll.AttributesElement)
+            //    {
+            //        Console.Write(" " + a.Key + "=" + a.Value + " ");
+            //    }
+            //    Console.WriteLine("/>");
+            //}
 
 
-            ArrayList m = xmi.Message;
-            foreach (Element mm in m)
-            {
-                Console.Write("\t<" + mm.Tag);
-                foreach (var a in mm.AttributesElement)
-                {
-                    Console.Write(" " + a.Key + "=" + a.Value + " ");
-                }
-                Console.WriteLine("/>");
-            }
-    
+            //ArrayList m = xmi.Message;
+            //foreach (Element mm in m)
+            //{
+            //    Console.Write("\t<" + mm.Tag);
+            //    foreach (var a in mm.AttributesElement)
+            //    {
+            //        Console.Write(" " + a.Key + "=" + a.Value + " ");
+            //    }
+            //    Console.WriteLine("/>");
+            //}
+
+
+            //ArrayList d = xmi.Diagram;
+            //foreach (Element dd in d)
+            //{
+            //    Console.Write("\t<" + dd.Tag);
+            //    foreach (var a in dd.AttributesElement)
+            //    {
+            //        Console.Write(" " + a.Key + "=" + a.Value + " ");
+            //    }
+            //    Console.WriteLine("/>");
+            //}
+
+            //ArrayList e = xmi.Element;
+            //foreach (Element ee in e)
+            //{
+            //    Console.Write("\t<" + ee.Tag);
+            //    foreach (var a in ee.AttributesElement)
+            //    {
+            //        Console.Write(" " + a.Key + "=" + a.Value + " ");
+            //    }
+            //    Console.WriteLine("/>");
+            //}
+
+
+            //ArrayList nomes = new ArrayList();
+            //ArrayList idades = new ArrayList();
+
+            //nomes.Add("filipe");
+            //nomes.Add("jane");
+            //nomes.Add("dayane");
+
+            //idades.Add("29");
+            //idades.Add("54");
+            //idades.Add("25");
+
+            //Dictionary<ArrayList, ArrayList> dic = new Dictionary<ArrayList, ArrayList>();
+            //dic.Add(nomes,idades);
+
+            //foreach( var a in dic)
+            //{
+            //    foreach( var aa in a.Key )
+            //    {
+            //        Console.WriteLine( aa );
+            //    }
+            //    foreach (var aa in a.Value)
+            //    {
+            //        Console.WriteLine(aa);
+            //    }
+            //}
 
         }
     }
--- VisualStudio/ConsoleApplication1/TesteXMI2/XMI.cs
+++ VisualStudio/ConsoleApplication1/TesteXMI2/XMI.cs
@@ -13,6 +13,9 @@ namespace TesteXMI2
         public const string OWNEDBEHAVIOR = "ownedBehavior";
         public const string LIFELINE = "lifeline";
         public const string MESSAGE = "message";
+        public const string DIAGRAM = "diagram";
+        public const string ELEMENTS = "elements";
+        public const string ELEMENT = "element";
             
         public string ArquivoXmi { get; private set; }
         public XmlDocument xmlDocument = new XmlDocument();
@@ -21,19 +24,60 @@ namespace TesteXMI2
         public ArrayList Lifeline { get; private set; }
         public ArrayList Message { get; private set; }
 
+                        //Diagram   Element
+        public Dictionary<ArrayList,ArrayList> Diagrams { get; private set; }
+        public ArrayList Diagram { get; private set; }
+        //public ArrayList Element { get; private set; }
+
         public XMI(string arquivoXmi)
         {
             this.OwnedBehavior = new ArrayList();
             this.Lifeline = new ArrayList();
             this.Message = new ArrayList();
+
+            this.Diagrams = new Dictionary<ArrayList, ArrayList>();
+            this.Diagram = new ArrayList();
+            //this.Element = new ArrayList();
               
             this.xmlDocument.Load(arquivoXmi);
 
             readElement("/" + OWNEDBEHAVIOR);
             readElement("/" + OWNEDBEHAVIOR + "/" + LIFELINE);
             readElement("/" + OWNEDBEHAVIOR + "/" + MESSAGE);
+            
+            readElement("/" + DIAGRAM);
+            //readElement("/" + DIAGRAM + "/" + ELEMENTS + "/" + ELEMENT);
         }
 
+
+        //READ THE ELEMENTS AND ADD IN YOUR ARRAYLIST
+        private void readElement(string tag)
+        {
+            ArrayList tagList = splitTags(tag);
+            int count = tagList.Count;
+            int lastIndex = count - 1;
+            string lastPosition = tagList[lastIndex].ToString();
+
+            ArrayList element = new ArrayList();
+
+            switch (lastPosition)
+            {
+                case OWNEDBEHAVIOR:     foreach (Element e in readElementAttributes(tag)) { this.OwnedBehavior.Add(e); } break;
+                case LIFELINE:          foreach (Element e in readElementAttributes(tag)) { this.Lifeline.Add(e); } break;
+                case MESSAGE:           foreach (Element e in readElementAttributes(tag)) { this.Message.Add(e); } break;
+                //case DIAGRAM:           foreach (Element e in readElementAttributes(tag)) { this.Diagram.Add(e); } break;
+                case ELEMENT:           foreach (Element e in readElementAttributes(tag)) { element.Add(e); } break;
+
+                case DIAGRAM:
+                    foreach (Element e in readElementAttributes(tag))
+                    {
+                        this.Diagram.Add(e);
+
+                    }
+                    break;
+            }
+
+        }
 
 
         //METHOD TO READ THE ELEMENT AND HIS ATTRIBUTES
@@ -69,24 +113,6 @@ namespace TesteXMI2
             }
             return elementList;
         }
-
-
-        //READ THE ELEMENTS AND ADD IN YOUR ARRAYLIST
-        private void readElement( string tag )
-        {
-            ArrayList tagList = splitTags(tag);
-            int count = tagList.Count;
-            int lastIndex = count - 1;
-            string lastPosition = tagList[lastIndex].ToString();
-
-            switch (lastPosition)
-            {
-                case OWNEDBEHAVIOR:     foreach (Element e in readElementAttributes(tag)) { this.OwnedBehavior.Add(e); } break;
-                case LIFELINE:          foreach (Element e in readElementAttributes(tag)) { this.Lifeline.Add(e); } break;
-                case MESSAGE:           foreach (Element e in readElementAttributes(tag)) { this.Message.Add(e); } break;
-            }
-        }
-
 
         //BREAK THE PATH OF TAG
         private ArrayList splitTags(string tag)
